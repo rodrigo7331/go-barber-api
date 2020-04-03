50d36a174e740a084e9059ad4f36b1ddf46dd5bb
rodrigo7331 <rodrigo7331@hotmail.com>
2020-04-03 02:31:25 -0300

    senha do User com hash

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -7,8 +7,11 @@ class UserController {
     if (userExists) {
       return res.status(400).json({ error: 'User already exists.' });
     }
-    const { name, email } = await User.create(req.body);
-    return res.json({ name, email });
+    const user = await User.create(req.body);
+    // create() não está retornando valores default da table, tive que usar reload --a documenação fala que ele retorna-- https://sequelize.org/master/manual/model-instances.html
+    const { name, email, provider } = await user.reload();
+
+    return res.json({ name, email, provider });
   }
 }
 
--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import { Model, Sequelize } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
@@ -7,11 +8,18 @@ class User extends Model {
       name: Sequelize.STRING,
       email: Sequelize.STRING,
       provider: Sequelize.BOOLEAN,
+      password: Sequelize.VIRTUAL,
       password_hash: Sequelize.STRING,
     },
     {
       sequelize,
     });
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+    return this;
   }
 }
 
